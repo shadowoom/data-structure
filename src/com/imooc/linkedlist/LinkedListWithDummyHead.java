940e4a35b96e86fdf403b74dc6cfefae395ac901
ZhangChen <ZhangChen>
2018-05-22 16:28:08 +0800

    add implementations of linked list

---
+++ src/com/imooc/linkedlist/LinkedListWithDummyHead.java
@@ -0,0 +1,81 @@
+package com.imooc.linkedlist;
+
+/**
+ * data-structure
+ * com.imooc.linkedlist
+ * Created by Zhang Chen
+ * 5/22/2018
+ */
+public class LinkedListWithDummyHead<E> {
+
+    private class Node {
+        public E e;
+        public Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+
+    }
+
+    private Node dummyHead;
+    private int size;
+
+    public LinkedListWithDummyHead() {
+        dummyHead = new Node(null, null);
+        size = 0;
+    }
+
+    public LinkedListWithDummyHead(E[] array) {
+        assert(array.length > 0);
+        Node current = new Node(array[0]);
+        dummyHead.next = current;
+        size = 1;
+        for(int i = 1; i < array.length; i++) {
+            current.next = new Node(array[i]);
+            current = current.next;
+            size++;
+        }
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void add(int index, E e) {
+        assert(index >= 0 && index <= size);
+        Node pre = dummyHead;
+        for(int i = 0; i < index; i++)
+            pre = pre.next;
+        pre.next = new Node(e, pre.next);
+        size++;
+
+    }
+
+    public void addFirst(E e) {
+        add(0, e);
+    }
+
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+}
